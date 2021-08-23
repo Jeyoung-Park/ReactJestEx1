75742aaa65660b2824c1d6ab69adcf2e5e8c0caf
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-08-24 00:16:15 +0900

    feat: App 컴포넌트 클래스형으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -31,52 +31,116 @@ const TodoListContainer = Styled.div`
   margin-bottom: 20px;
 `;
 
-const App = () => {
-  const [todo, setTodo] = useState("");
-  const [todoList, setTodoList] = useState<string[]>([]);
+interface Props {}
 
-  const addTodo = (): void => {
+interface State {
+  readonly todo: string;
+  readonly todoList: string[];
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      todo: "",
+      todoList: [],
+    };
+  }
+
+  private addTodo = (): void => {
+    const { todo, todoList } = this.state;
     if (todo) {
-      setTodoList([...todoList, todo]);
-      setTodo("");
+      this.setState({
+        todo: "",
+        todoList: [...todoList, todo],
+      });
     }
   };
 
-  const deleteTodo = (index: number): void => {
-    let list = [...todoList];
+  private deleteTodo = (index: number): void => {
+    let list = [...this.state.todoList];
     list.splice(index, 1);
-    setTodoList(list);
+    this.setState({
+      todoList: list,
+    });
   };
 
-  return (
-    <Container>
-      <Contents>
-        <TodoListContainer data-testid='todoList'>
-          {todoList.map((item, index) => (
-            <TodoItem
-              key={item}
-              label={item}
-              onDelete={() => deleteTodo(index)}
-            />
-          ))}
-        </TodoListContainer>
+  render() {
+    const { todo, todoList } = this.state;
 
-        <InputContainer>
-          <Input
-            placeholder="할 일을 입력해 주세요"
-            onChange={(text) => setTodo(text)}
-            value={todo}
-          />
-          <Button
-            label="추가"
-            backgroundColor="red"
-            hoverColor="green"
-            onClick={addTodo}
-          />
-        </InputContainer>
-      </Contents>
-    </Container>
-  );
-};
+    return (
+      <Container>
+        <Contents>
+          <TodoListContainer data-testid="todoList">
+            {todoList.map((item, index) => (
+              <TodoItem
+                key={item}
+                label={item}
+                onDelete={() => this.deleteTodo(index)}
+              />
+            ))}
+          </TodoListContainer>
+          <InputContainer>
+            <Input
+              placeholder="항 일을 입력해 주세요"
+              value={todo}
+              onChange={(text) => this.setState({ todo: text })}
+            />
+            <Button label="추가" onClick={this.addTodo} />
+          </InputContainer>
+        </Contents>
+      </Container>
+    );
+  }
+}
+
+// const App = () => {
+//   const [todo, setTodo] = useState("");
+//   const [todoList, setTodoList] = useState<string[]>([]);
+
+//   const addTodo = (): void => {
+//     if (todo) {
+//       setTodoList([...todoList, todo]);
+//       setTodo("");
+//     }
+//   };
+
+//   const deleteTodo = (index: number): void => {
+//     let list = [...todoList];
+//     list.splice(index, 1);
+//     setTodoList(list);
+//   };
+
+//   return (
+//     <Container>
+//       <Contents>
+//         <TodoListContainer data-testid='todoList'>
+//           {todoList.map((item, index) => (
+//             <TodoItem
+//               key={item}
+//               label={item}
+//               onDelete={() => deleteTodo(index)}
+//             />
+//           ))}
+//         </TodoListContainer>
+
+//         <InputContainer>
+//           <Input
+//             placeholder="할 일을 입력해 주세요"
+//             onChange={(text) => setTodo(text)}
+//             value={todo}
+//           />
+//           <Button
+//             label="추가"
+//             backgroundColor="red"
+//             hoverColor="green"
+//             onClick={addTodo}
+//           />
+//         </InputContainer>
+//       </Contents>
+//     </Container>
+//   );
+// };
 
 export default App;
